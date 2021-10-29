4a3913db5e323e2fa36623c4283305e978f87421
kazimyilmaz <kazimyillmaz@gmail.com>
2021-10-30 00:35:08 +0300

    Create README.md

---
+++ src/app.js
@@ -0,0 +1,22 @@
+console.log("Merhaba kodlama.io ")
+//camelCasing 
+
+let dolarBugun = 9.30 
+let dolarDun = 9.20 
+
+{
+let dolarDun = 9.10
+}
+console.log(dolarDun)
+const euroDun = 11.2 
+
+console.log(euroDun)
+
+ let konutKredileri = ["Konut kredisi,Emlak Konut Kredisi,Kamu Konut Kredisi,Özel konut kredisi"]
+console.log("<ul>")
+ for(let i = 0;i<konutKredileri.lenght;i++){
+  console.log("<li>"+konutKredileri[i]+"</li>")
+ }
+
+console.log("</ul>")
+console.log(konutKredileri)   
